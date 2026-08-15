91d0ec9377e0947e57001c58c44e179b26a02c76
engbetsy <betsykawira52@gmail.com>
2026-08-15 21:42:15 +0300

    initial files

---
+++ c-output.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!"); // put the double quotes
+  printf("I am learning C.");
+  printf("And it is awesome!"); // we use printf to output
+  return 0;
+}
---
+++ change-values.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+
+int myNum = 20;
+
+int myOtherNum = 26;
+
+// Assign the value of myOtherNum (26) to myNum
+myNum = myOtherNum;
+
+// myNum is now 26 (was 20)
+printf("%d", myNum);
+return 0;
+}
